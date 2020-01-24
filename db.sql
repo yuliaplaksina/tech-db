d16d09b335ae34be041b68fbed6ab74f837960bc
yuliaplaksina <plaksina-yulia@yandex.ru>
2020-01-24 18:47:08 +0300

    db

--- db.sql
+++ db.sql
@@ -1,25 +1,21 @@
-
-
+--
+--
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-SET default_tablespace = '';
 
-SET default_with_oids = false;
-
-CREATE EXTENSION IF NOT EXISTS citext with schema public;
+CREATE EXTENSION citext;
 
 -- forum
 
-CREATE TABLE public.forum (
+CREATE TABLE forum (
       id integer NOT NULL PRIMARY KEY,
       slug citext NOT NULL,
       threads integer DEFAULT 0 NOT NULL,
@@ -29,9 +25,9 @@ CREATE TABLE public.forum (
 );
 
 
-ALTER TABLE public.forum OWNER TO postgres;
+ALTER TABLE forum OWNER TO postgres;
 
-CREATE SEQUENCE public.forum_id_seq
+CREATE SEQUENCE forum_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -40,24 +36,24 @@ CREATE SEQUENCE public.forum_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.forum_id_seq OWNER TO postgres;
-ALTER SEQUENCE public.forum_id_seq OWNED BY public.forum.id;
+ALTER TABLE forum_id_seq OWNER TO postgres;
+ALTER SEQUENCE forum_id_seq OWNED BY forum.id;
 
 
-CREATE UNIQUE INDEX forum_slug_uindex ON public.forum USING btree (lower(slug));
+CREATE UNIQUE INDEX forum_slug_uindex ON forum USING btree (slug);
 
 
-CREATE TABLE public.forum_user (
+CREATE TABLE forum_user (
        forum_id integer NOT NULL,
        user_id integer NOT NULL
 );
 
 
-ALTER TABLE public.forum_user OWNER TO postgres;
+ALTER TABLE forum_user OWNER TO postgres;
 
 -- post
 
-CREATE TABLE public.post (
+CREATE TABLE post (
      id integer NOT NULL PRIMARY KEY,
      author citext NOT NULL,
      created text NOT NULL,
@@ -69,9 +65,9 @@ CREATE TABLE public.post (
      path bigint[] DEFAULT '{0}'::bigint[] NOT NULL
 );
 
-ALTER TABLE public.post OWNER TO postgres;
+ALTER TABLE post OWNER TO postgres;
 
-CREATE SEQUENCE public.post_id_seq
+CREATE SEQUENCE post_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -79,19 +75,19 @@ CREATE SEQUENCE public.post_id_seq
     NO MAXVALUE
     CACHE 1;
 
-ALTER TABLE public.post_id_seq OWNER TO postgres;
-ALTER SEQUENCE public.post_id_seq OWNED BY public.post.id;
+ALTER TABLE post_id_seq OWNER TO postgres;
+ALTER SEQUENCE post_id_seq OWNED BY post.id;
 
 
-CREATE INDEX post_author_forum_index ON public.post USING btree (author, forum);
-CREATE INDEX post_forum_index ON public.post USING btree (forum);
-CREATE INDEX post_parent_index ON public.post USING btree (parent);
-CREATE INDEX post_path_index ON public.post USING gin (path);
-CREATE INDEX post_thread_index ON public.post USING btree (thread);
+CREATE INDEX post_author_forum_index ON post USING btree (author, forum);
+CREATE INDEX post_forum_index ON post USING btree (forum);
+CREATE INDEX post_parent_index ON post USING btree (parent);
+CREATE INDEX post_path_index ON post USING gin (path);
+CREATE INDEX post_thread_index ON post USING btree (thread);
 
 -- thread
 
-CREATE TABLE public.thread (
+CREATE TABLE thread (
        id integer NOT NULL PRIMARY KEY,
        author citext NOT NULL,
        created timestamp with time zone DEFAULT now() NOT NULL,
@@ -102,9 +98,9 @@ CREATE TABLE public.thread (
        votes integer DEFAULT 0
 );
 
-ALTER TABLE public.thread OWNER TO postgres;
+ALTER TABLE thread OWNER TO postgres;
 
-CREATE SEQUENCE public.thread_id_seq
+CREATE SEQUENCE thread_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -112,17 +108,17 @@ CREATE SEQUENCE public.thread_id_seq
     NO MAXVALUE
     CACHE 1;
 
-ALTER TABLE public.thread_id_seq OWNER TO postgres;
-ALTER SEQUENCE public.thread_id_seq OWNED BY public.thread.id;
+ALTER TABLE thread_id_seq OWNER TO postgres;
+ALTER SEQUENCE thread_id_seq OWNED BY thread.id;
 
 
-CREATE INDEX thread_forum_index ON public.thread USING btree (forum);
-CREATE UNIQUE INDEX thread_id_uindex ON public.thread USING btree (id);
-CREATE INDEX thread_slug_index ON public.thread USING btree (slug);
+CREATE INDEX thread_forum_index ON thread USING btree (forum);
+CREATE UNIQUE INDEX thread_id_uindex ON thread USING btree (id);
+CREATE INDEX thread_slug_index ON thread USING btree (slug);
 
 --- user
 
-CREATE TABLE public."user" (
+CREATE TABLE "user" (
        id integer NOT NULL PRIMARY KEY,
        nick_name citext NOT NULL,
        email citext NOT NULL,
@@ -131,9 +127,9 @@ CREATE TABLE public."user" (
 );
 
 
-ALTER TABLE public."user" OWNER TO postgres;
+ALTER TABLE "user" OWNER TO postgres;
 
-CREATE SEQUENCE public.user_id_seq
+CREATE SEQUENCE user_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -141,43 +137,41 @@ CREATE SEQUENCE public.user_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.user_id_seq OWNER TO postgres;
-ALTER SEQUENCE public.user_id_seq OWNED BY public."user".id;
+ALTER TABLE user_id_seq OWNER TO postgres;
+ALTER SEQUENCE user_id_seq OWNED BY "user".id;
 
-CREATE UNIQUE INDEX user_email_uindex ON public."user" USING btree (email);
-CREATE UNIQUE INDEX user_nick_name_uindex ON public."user" USING btree (nick_name);
-CREATE INDEX user_index ON public."user" USING btree (nick_name, email, full_name, about);
+CREATE UNIQUE INDEX user_email_uindex ON "user" USING btree (email);
+CREATE UNIQUE INDEX user_nick_name_uindex ON "user" USING btree (nick_name);
+CREATE INDEX user_index ON "user" USING btree (nick_name, email, full_name, about);
 
 
-CREATE TABLE public.vote (
+CREATE TABLE vote (
      user_id integer,
      voice integer NOT NULL,
      thread_id integer NOT NULL
 );
 
 
-ALTER TABLE public.vote OWNER TO postgres;
+ALTER TABLE vote OWNER TO postgres;
 
 ----
 
-ALTER TABLE ONLY public.forum ALTER COLUMN id SET DEFAULT nextval('public.forum_id_seq'::regclass);
+ALTER TABLE ONLY forum ALTER COLUMN id SET DEFAULT nextval('forum_id_seq'::regclass);
 
-ALTER TABLE ONLY public.post ALTER COLUMN id SET DEFAULT nextval('public.post_id_seq'::regclass);
+ALTER TABLE ONLY post ALTER COLUMN id SET DEFAULT nextval('post_id_seq'::regclass);
 
-ALTER TABLE ONLY public.thread ALTER COLUMN id SET DEFAULT nextval('public.thread_id_seq'::regclass);
+ALTER TABLE ONLY thread ALTER COLUMN id SET DEFAULT nextval('thread_id_seq'::regclass);
 
-ALTER TABLE ONLY public."user" ALTER COLUMN id SET DEFAULT nextval('public.user_id_seq'::regclass);
+ALTER TABLE ONLY "user" ALTER COLUMN id SET DEFAULT nextval('user_id_seq'::regclass);
 
 -------
 
-SELECT pg_catalog.setval('public.forum_id_seq', 1, false);
+SELECT pg_catalog.setval('forum_id_seq', 1, false);
 
-SELECT pg_catalog.setval('public.post_id_seq', 1, false);
+SELECT pg_catalog.setval('post_id_seq', 1, false);
 
-SELECT pg_catalog.setval('public.thread_id_seq', 1, false);
+SELECT pg_catalog.setval('thread_id_seq', 1, false);
 
-SELECT pg_catalog.setval('public.user_id_seq', 1, false);
+SELECT pg_catalog.setval('user_id_seq', 1, false);
 
 ------
-
-GRANT ALL ON SCHEMA public TO PUBLIC;
